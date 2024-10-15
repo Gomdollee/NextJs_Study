b18f169f2d9c51f3d4f66e280233936c30b31999
junwon_lee <wjdrbs307@naver.com>
2024-10-16 00:13:53 +0900

    map 사용

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,13 +1,22 @@
 export default function List() {
+    let goods = ['Tomatoes', 'Pasta', 'Coconut']
+    // let array = [2,3,4]
+    // array.map((a, i)=>{
+    //     console.log(a)
+    // })
+
     return (
       <div>
-        <h2>Products</h2>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
-        <div className="food">
-          <h4>상품명 $40</h4>
-        </div>
+        <h4 className="title">상품목록</h4>
+        {
+            goods.map((a, i) => {
+                return (
+                <div className="food">
+                    <h4>{goods[i]} $20</h4>
+                  </div>
+                )
+            })
+        }
       </div>
     )
   } 
