3115bfb90dec42ded7f8aeaa3ffb4c0dd4434d57
junwon_lee <wjdrbs307@naver.com>
2024-10-24 22:24:13 +0900

    하루 커밋

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,17 +1,30 @@
+import {age , name} from './data.js'
+let Basket = ['Tomatoes', 'Pasta']
+
 export default function Cart() {
     return (
       <div>
         <h4 className="title">Cart</h4>
         <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
+          <p>상품명 {age}</p>
+          <p>$40 {name}</p>
           <p>1개</p>
         </div>
-        <div className="cart-item">
-          <p>상품명</p>
-          <p>$40</p>
-          <p>1개</p>
-        </div>
-      </div>
+        <CartItem product={Basket[0]}/>
+        <CartItem/>
+    </div>
     )
-  } 
+}
+
+
+function CartItem(props) {
+    return(
+        <div className="cart-item">
+        <p>상품명 {props.product} </p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+ 
+
+    )
+}
